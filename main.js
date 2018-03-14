f05144d72eb3cc732235e8c664e29bd70bc6d3e8
Russell Rosario <rosariorussell@gmail.com>
2018-03-14 16:53:47 -0400

    added firebase

---
+++ main.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyA48-luSX_-ES8g2zYJEGgomzgmZ52Jxv8",
+    authDomain: "train-scheduler-bde89.firebaseapp.com",
+    databaseURL: "https://train-scheduler-bde89.firebaseio.com",
+    projectId: "train-scheduler-bde89",
+    storageBucket: "",
+    messagingSenderId: "862435383775"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
